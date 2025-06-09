2fc49a8ebd22a8d2af702812f61e995b3c6f28f0
Abhinav Shukla <67401627+maxprogrammer007@users.noreply.github.com>
2025-06-09 16:11:53 +0530

    Create olin.py

---
+++ src/olin.py
@@ -0,0 +1,4 @@
+# src/olin.py
+# This module will implement the regenerative OLIN algorithm.
+# It will use a sliding window and call the `build_IN` function from
+# information_network.py for every new window of data.
