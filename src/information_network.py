d9ef560bb3a83cfa38fb6e614050cb87ab11c2c0
Abhinav Shukla <67401627+maxprogrammer007@users.noreply.github.com>
2025-06-09 16:11:51 +0530

    Create information_network.py

---
+++ src/information_network.py
@@ -0,0 +1,6 @@
+# src/information_network.py
+# This module will contain the implementation of the core Information Network (IN) algorithm.
+# Key functions:
+# - build_IN(training_data, attributes, target_attribute)
+# - calculate_mutual_information(data_subset, attribute, target)
+# - is_split_significant(data_subset, attribute, target) -> bool (using chi-square test)
